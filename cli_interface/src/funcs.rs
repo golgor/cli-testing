93e4b95f44903f978e3b916827d27d628924937d
Robert Nyström <robert.nystrom@toolsense.io>
2023-12-27 22:20:31 +0100

    added tests

---
+++ cli_interface/src/funcs.rs
@@ -0,0 +1,30 @@
+pub const VARIABEL: i32 = 5;
+
+pub fn testare(z: i32) -> i32 {
+    5 + z + VARIABEL
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_valid_return_from_testare() -> Result<(), String> {
+        let result = testare(2);
+        if result == 12 {
+            Ok(())
+        } else {
+            Err(format!("Expected 12, got {}", result))
+        }
+    }
+
+    #[test]
+    fn check_valid_return_from_testare_2() -> Result<(), String> {
+        let result = testare(5);
+        if result == 15 {
+            Ok(())
+        } else {
+            Err(format!("Expected 15, got {}", result))
+        }
+    }
+}
--- message_assembler/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Hello, world, this is a message from a dev-container!");
-}
-
-// rustup component add clippy
